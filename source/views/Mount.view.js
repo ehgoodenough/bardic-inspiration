6bb03c3b00711fe09c1b06398e72d63c3a98fc32
Andrew Goodenough <ehgoodenough@gmail.com>
2023-01-17 20:28:07 +0200

    Imported some basic boilerplate.

---
+++ source/views/Mount.view.js
@@ -0,0 +1,19 @@
+import * as Preact from "preact"
+
+import index from "index"
+
+import Frame from "views/Frame.view.js"
+import "views/Mount.view.less"
+
+export default class Mount {
+    render() {
+        return (
+            <div className="Mount">
+                <Frame size="16x9">
+                    <img src={require("images/spacebar.png")}/>
+                    <div>{index.time.toFixed(3)}s</div>
+                </Frame>
+            </div>
+        )
+    }
+}
