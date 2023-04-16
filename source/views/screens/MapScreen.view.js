bfb1c9a590991cd8fe9a0e189d956fe3e9fa4a49
Andrew Goodenough <ehgoodenough@gmail.com>
2023-04-16 17:57:54 +0100

    Added little dots for the character units.

--- source/views/screens/MapScreen.view.js
+++ source/views/screens/MapScreen.view.js
@@ -1,7 +1,19 @@
+import Poin from "poin"
 import * as Preact from "preact"
 import Data from "models/Data.js"
 
 import "views/screens/MapScreen.view.less"
+
+const UNITS = [
+    {
+        "name": "Cuthos",
+        "color": "yellow",
+        "position": {
+            "x": -16,
+            "y": +32,
+        }
+    }
+]
 
 export default class MapScreen {
     render() {
@@ -12,10 +24,69 @@ export default class MapScreen {
                 <div class="Map" style={{
                     "background-image": "url(" + require("images/map.png") + ")"
                 }}/>
+                <div class="Units">
+                    {UNITS.map((unit) => {
+                        return <Unit unit={unit}/>
+                    })}
+                </div>
             </div>
         )
     }
 }
+
+let selectedUnit = undefined
+
+class Unit {
+    render() {
+        return (
+            <div class="Unit" style={this.style}
+                onMouseDown={this.onMouseDown}
+                onMouseUp={this.onMouseUp}>
+                <div class="Shadow"/>
+                <div class="Circle" style={{
+                    "background-color": this.props.unit.color,
+                }}/>
+                <div class="Name">
+                    <span>{this.props.unit.name}</span>
+                </div>
+            </div>
+        )
+    }
+    get style() {
+        if(selectedUnit == this.props.unit) {
+            return {
+                "top": Poin.position.y + "px",
+                "left":  Poin.position.x + "px",
+                "position": "fixed",
+            }
+        } else {
+            return {
+                "top": this.props.unit.position.y + "px",
+                "left":  this.props.unit.position.x + "px",
+                "position": "absolute",
+            }
+        }
+    }
+    get onMouseDown() {
+        return (event) => {
+            selectedUnit = this.props.unit
+        }
+    }
+}
+
+window.addEventListener("contextmenu", function(event) {
+    event.preventDefault()
+    selectedUnit = undefined
+})
+
+window.addEventListener("mouseup", function(event) {
+    event.preventDefault()
+    if(selectedUnit != undefined) {
+        selectedUnit.position.x = Poin.position.x - (window.innerWidth / 2)
+        selectedUnit.position.y = Poin.position.y - (window.innerHeight / 2)
+        selectedUnit = undefined
+    }
+})
 
 class Screenshot {
     render() {
