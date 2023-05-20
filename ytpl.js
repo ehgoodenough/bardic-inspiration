2c007abb2e3b1979ff6c1d0a811cd5ef85e70382
Andrew Goodenough <ehgoodenough@gmail.com>
2023-05-20 11:40:59 +0200

    Synced the youtube playlists.

--- ytpl.js
+++ ytpl.js
@@ -11,8 +11,6 @@ let playlistUrls = [
     "https://www.youtube.com/playlist?list=PLV8C9t518pCLJVekMt-g8kIx4-4mibhDm",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCJXjNed2p60cLVcWXDQCeQ6",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCKAKuh-e4jUrbMNLh8Lcpfz",
-    "https://www.youtube.com/playlist?list=PLV8C9t518pCJVX2o3tmvpYJhWADDl2DnG",
-    "https://www.youtube.com/playlist?list=PLV8C9t518pCInIaeVhXxFiHdcVctaDDqB",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCJ7Ymh3qHv5iqha9F-3n2J7",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCKUyv6nH6e6-8ki3aj_CUzD",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCIXlZj1-Hqn4LMK_JjPVutV",
@@ -27,6 +25,13 @@ let playlistUrls = [
     "https://www.youtube.com/playlist?list=PLV8C9t518pCL2BTTDpXnQvRufDK8dcZTw",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCLk03um_zlF6VHTXnnCgNXu",
     "https://www.youtube.com/playlist?list=PLV8C9t518pCIZrVv8dtEm1RXdO-9f2p8p",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCJK9v2PRNKae-eDarx7XKjM",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCJVX2o3tmvpYJhWADDl2DnG",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCInIaeVhXxFiHdcVctaDDqB",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCJKX2IUbhzpPjOcp-6N1UIK",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCLmTO_i-VqU_dFDjsJePSFa",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCL0epmuVHaw2QrSrf_SBGKi",
+    "https://www.youtube.com/playlist?list=PLV8C9t518pCLr6cZxuSMIXWnUG9ZufxCJ",
 ]
 
 let playlists = await Promise.all(playlistUrls.map((playlistUrl) => {
