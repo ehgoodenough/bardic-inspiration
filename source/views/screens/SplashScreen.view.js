404bf2b9d2b8f2ff93c46aacf4c687f6aaf7d9fc
Andrew Goodenough <ehgoodenough@gmail.com>
2023-12-04 14:02:08 +0900

    Added routing to different rooms from the splash.

--- source/views/screens/SplashScreen.view.js
+++ source/views/screens/SplashScreen.view.js
@@ -1,5 +1,6 @@
 import * as Preact from "preact"
 import Navigation from "models/Navigation.js"
+import Data from "models/Data.js"
 
 const random = Math.ceil(Math.random() * 3)
 
@@ -8,21 +9,48 @@ import "views/screens/SplashScreen.view.less"
 export default class SplashScreen {
     render() {
         return (
-            <div class="SplashScreen" onClick={this.onClick}>
+            <div class="SplashScreen">
                 <div class="Background" random={random}/>
                 <div class="Prompt">
                     <div class="Text">
                         Bardic<br/>Inspiration
                     </div>
-                    <div class="Icon">
-                        <div class="fill"/>
-                        <div class="shape material-icons">smart_display</div>
-                    </div>
+                    {this.content}
                 </div>
             </div>
         )
     }
-    onClick() {
-        Navigation.go("/campaigns/theros")
+    get content() {
+        if(Data.campaign.key == undefined) {
+            return (
+                <form onSubmit={this.onSubmitForm}>
+                    <input type="text" placeholder="Name of Campaign"/>
+                </form>
+            )
+        }
+        return (
+            <div class="Icon" onClick={this.onClickButton}>
+                <div class="fill"/>
+                <div class="shape material-icons">smart_display</div>
+            </div>
+        )
+    }
+    get onSubmitForm() {
+        return (event) => {
+            event.preventDefault()
+            const value = event.target.children[0].value
+            console.log(value.toLowerCase())
+            if(value.toLowerCase() != "theros"
+            && value.toLowerCase() != "cthulu") {
+                window.alert("Could not find campaign " + value)
+                return
+            }
+            Navigation.go("/campaigns/" + value.toLowerCase())
+        }
+    }
+    get onClickButton() {
+        return (event) => {
+            // Navigation.run("/campaigns/" + Data.campaign.key)
+        }
     }
 }
