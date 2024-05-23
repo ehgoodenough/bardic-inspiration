41c04764b9a06c8755008df237f72d12a88d0694
Andrew Goodenough <ehgoodenough@gmail.com>
2024-05-22 17:33:39 -0700

    Supported youtube behind local audio.

--- source/models/AudioPlayer.js
+++ source/models/AudioPlayer.js
@@ -1,10 +1,13 @@
 import IO from "models/IO.js"
 import Data from "models/Data.js"
+import Something from "models/Something.js"
+
+import YoutubePlayer from "models/YoutubePlayer.js"
 
 export default class AudioPlayer {
     constructor(streamId) {
         this.streamId = streamId
-        this.audio = new Audio()
+        this.backupYoutubePlayer = new YoutubePlayer(streamId)
     }
     load(run) {
         if(run.url == undefined) {
@@ -14,66 +17,78 @@ export default class AudioPlayer {
                 return
             }
         }
-        console.log(">", run)
-        this.audio.pause()
+        if(this.audio != undefined) this.audio.pause()
         this.audio = new Audio(run.url)
-        this.setVolume(Data.campaign.streams[this.streamId].volume)
-        this.audio.autoplay = true
-        this.audio.currentTime = Math.floor(run.currentTime / 1000) || 0.1
-        // await new Promise((resolve) => {
-        //     this.audio.
-        // })
-        // this.play()
-        // EVENT LISTENERS?
 
-        // if(event.data == YT.PlayerState.PAUSED
-        // && Data.campaign.streams[this.streamId]?.run?.state != "paused") {
-        //     this.seek(computeCurrentTime(Data.campaign.streams["a"].run))
-        //     this.play()
-        //     return
-        // }
-        // if(event.data == YT.PlayerState.PLAYING
-        // && Data.campaign.streams[this.streamId]?.run?.state != "playing") {
-        //     this.seek(computeCurrentTime(Data.campaign.streams["a"].run))
-        //     this.pause()
-        //     return
-        // }
-        //
-        // if(event.data == YT.PlayerState.ENDED) {
-        //     // if(this.streamdId != "a") return
-        //     if(Data.campaign.streams[this.streamId].queue == undefined) return
-        //     if(Data.campaign.streams[this.streamId].queue.length == 0) return
-        //     const currentMusic = Data.campaign.streams[this.streamId].queue.find((music) => {
-        //         return Data.campaign.streams[this.streamId].run.queueId == music.queueId
-        //     })
-        //     const currentIndex = Data.campaign.streams[this.streamId].queue.indexOf(currentMusic)
-        //     const nextIndex = currentIndex + 1
-        //     const nextMusic = Data.campaign.streams[this.streamId].queue[nextIndex]
-        //     if(nextMusic == undefined) return
-        //     const TIME_BETWEEN_SONGS = 500
-        //     Something.updateCurrentRun(this.streamId, {
-        //         "queueId": nextMusic.queueId,
-        //         "youtubeId": nextMusic.youtubeId,
-        //         "startTime": Date.now() + TIME_BETWEEN_SONGS,
-        //         "state": "playing",
-        //     })
-        // }
+        this.audio.addEventListener("error", (event) => {
+            if(this.audio.error.code == 4) {
+                delete this.audio
+                this.backupYoutubePlayer.load(run)
+            }
+        })
+
+        this.audio.addEventListener("loadstart", (event) => {
+            this.audio.autoplay = true
+            this.audio.currentTime = Math.floor(run.currentTime / 1000) || 0.1
+
+            const volume = Data.campaign.streams[this.streamId].volume
+            let volumeLevel = volume.level * IO.volume.level
+            if(volumeLevel < 0.01) volumeLevel = 0
+            if(volumeLevel > 0.99) volumeLevel = 1
+            this.audio.volume = volumeLevel
+            if((volume.isMuted == true || IO.volume.isMuted == true)
+            && this.audio.muted != true) {
+                this.audio.muted = true
+            }
+            if((volume.isMuted != true && IO.volume.isMuted != true)
+            && this.audio.muted == true) {
+                this.audio.muted = false
+            }
+
+            this.audio.addEventListener("ended", (event) => {
+                if(Data.campaign.streams[this.streamId].queue == undefined) return
+                if(Data.campaign.streams[this.streamId].queue.length == 0) return
+                const currentMusic = Data.campaign.streams[this.streamId].queue.find((music) => {
+                    return Data.campaign.streams[this.streamId].run.queueId == music.queueId
+                })
+
+                // if(this.streamdId != "a") return
+                const currentIndex = Data.campaign.streams[this.streamId].queue.indexOf(currentMusic)
+                const nextIndex = currentIndex + 1
+                const nextMusic = Data.campaign.streams[this.streamId].queue[nextIndex]
+                if(nextMusic == undefined) return
+                const TIME_BETWEEN_SONGS = 500
+                Something.updateCurrentRun(this.streamId, {
+                    "queueId": nextMusic.queueId,
+                    "youtubeId": nextMusic.youtubeId,
+                    "startTime": Date.now() + TIME_BETWEEN_SONGS,
+                    "state": "playing",
+                })
+            })
+        })
     }
     seek({currentTime}) {
+        if(this.audio == undefined) return this.backupYoutubePlayer.seek({currentTime})
         this.audio.currentTime = Math.floor(currentTime / 1000) || 0.1
     }
     pause() {
-        this.audio.pause()
+        this.backupYoutubePlayer.pause()
+        if(this.audio != undefined) {
+            this.audio.pause()
+        }
     }
     play() {
+        if(this.audio == undefined) return this.backupYoutubePlayer.play()
         this.audio.play()
     }
     // returns in milliseconds
     get duration() {
+        if(this.audio == undefined) return this.backupYoutubePlayer.duration
         return this.audio.duration * 1000
     }
     // volume is 0 to 100
     setVolume(volume = {"level": 0}) {
+        if(this.audio == undefined) return this.backupYoutubePlayer.setVolume(volume)
         let volumeLevel = volume.level * IO.volume.level
         if(volumeLevel < 0.01) volumeLevel = 0
         if(volumeLevel > 0.99) volumeLevel = 1
--- source/models/IO.js
+++ source/models/IO.js
@@ -1,6 +1,5 @@
 import Data from "models/Data.js"
 import AudioPlayer from "models/AudioPlayer.js"
-import YoutubePlayer from "models/YoutubePlayer.js"
 
 const DEFAULT_VOLUME_LEVEL = 0.5
 
@@ -8,8 +7,6 @@ export default new class IO {
     constructor() {
         this.streams = {
             "a": new AudioPlayer("a"),
-            // "b": new YoutubePlayer("b"),
-            // "c": new AudioPlayer("c"),
         }
 
         this.volume = {
